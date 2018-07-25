5cfdbb7966abda7715478bfb0939cb566d4902bd
Adam Carballido <carballido.adam@yahoo.com>
2018-07-25 19:37:46 -0400

    Tank now turns left and right

--- Source/BattleTank/Public/TankMovementComponent.h
+++ Source/BattleTank/Public/TankMovementComponent.h
@@ -16,11 +16,17 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable, Category = Input)
+	UFUNCTION(BlueprintCallable, Category = Setup)
 		void IntendMoveForward(float Throw);
 
-	UFUNCTION(BlueprintCallable, Category = Setup)
+	UFUNCTION(BlueprintCallable, Category = Input)
 		void Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
+
+	UFUNCTION(BlueprintCallable, Category = Input)
+		void IntendTurnRight(float Throw);
+
+	UFUNCTION(BlueprintCallable, Category = Input)
+		void IntendTurnLeft(float Throw);
 
 private:
 	UTankTrack* LeftTrack = nullptr;
